b7c115ada300181f6ee916636e5633525b34b66f
Mads Kalør <mads@kaloer.com>
2014-02-05 14:39:16 +0100

    Plot script added

---
+++ plot.R
@@ -0,0 +1,26 @@
+library(ggplot2)
+library(maps)
+
+# Load map data
+world<-map_data('world')
+
+# Generates plot of the locations on a world map.
+plotworld <- function(df) {
+    # Set theme.
+    theme_opts <- list(theme(panel.background = element_blank(),
+                             panel.grid.major = element_blank(),
+                             panel.grid.minor = element_blank(),
+                             axis.text.x = element_blank(),
+                             axis.text.y = element_blank(),
+                             axis.ticks = element_blank(),
+                             plot.background = element_rect(fill="gray6"),
+                             legend.position = "none"))
+    # Plot
+    p <- ggplot(legend=FALSE) +
+        geom_polygon(data=world, aes(x=long, y=lat,group=group), fill="gray12") +
+        theme_opts +
+        xlab("") + ylab("") + 
+        geom_point(data=df, aes(lat,long),
+            size=0.1, alpha=0.1, colour="white")
+    return(p)
+}
