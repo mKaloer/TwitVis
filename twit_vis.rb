b8345726b165c32dfcce931c56d6d20900e86ae5
Mads Kalør <mads@kaloer.com>
2014-02-07 22:33:59 +0100

    Main class clarifications

--- twit_vis.rb
+++ twit_vis.rb
@@ -1,11 +1,17 @@
 require 'yaml'
-require 'twitter'
+require_relative 'twitter_miner.rb'
 
+# Create an auth.yml file with your Twitter api keys:
+
+#:consumer_key: YOUR_CONSUMER_KEY_HERE
+#:consumer_secret: YOUR_CONSUMER_SECRET_HERE
+#:access_token: YOUR_ACCESS_TOKEN_HERE
+#:access_token_secret: YOUR_ACCESS_TOKEN_SECRET_HERE
 auth = YAML::load(File.open('auth.yml'))
 
-client = Twitter::Streaming::Client.new do |config|
-  config.consumer_key        = auth['consumer_key']
-  config.consumer_secret     = auth['consumer_secret']
-  config.access_token        = auth['access_token']
-  config.access_token_secret = auth['access_token_secret']
-end
+twitter = TwitterMiner.new
+
+# Visualize olympc tweets.
+topics = ["olympics", "sochi"]
+twitter.visualize_real_time_topics(auth, topics)
+#twitter.visualize_historical_topics(auth, "olympics", 100)
