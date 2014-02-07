c77fff325391b5c9d4df7ac71974db3814891aa0
Mads Kalør <mads@kaloer.com>
2014-02-07 23:02:27 +0100

    Command line argument parsing added

--- twit_vis.rb
+++ twit_vis.rb
@@ -1,5 +1,38 @@
 require 'yaml'
+require 'optparse'
 require_relative 'twitter_miner.rb'
+
+options = {}
+OptionParser.new do |opts|
+  options[:output] = "plots"
+  options[:real_time] = true
+  
+  opts.on("-t", "--topics X,Y,Z", Array, "The topics to analyze") do |list|
+    options[:topics] = list
+  end
+  
+  opts.on("-o", "--output [OUTPUT_DIR]","The output image output directory") do |out|
+    options[:output] = out
+  end
+  
+  opts.on("-r", "--realtime","If set, a real time analysis will be performed") do |rt|
+    options[:real_time] = true
+  end
+  
+  opts.on("-h", "--historical","If set, a historical time analysis will be performed") do |rt|
+    options[:historical] = true
+  end
+  
+  opts.on_tail("-h", "--help", "Show help") do
+    puts opts
+    exit
+  end
+end.parse!
+
+if options[:topics].nil?
+  puts "Missing argument -t: Topics required. Use -h for help."
+  exit
+end
 
 # Create an auth.yml file with your Twitter api keys:
 
@@ -11,7 +44,8 @@ auth = YAML::load(File.open('auth.yml'))
 
 twitter = TwitterMiner.new
 
-# Visualize olympc tweets.
-topics = ["olympics", "sochi"]
-twitter.visualize_real_time_topics(auth, topics)
-#twitter.visualize_historical_topics(auth, "olympics", 100)
+if options[:historical]
+  twitter.visualize_historical_topics(auth, options[:topics].join(","), 100, options[:output])
+else
+  twitter.visualize_real_time_topics(auth, options[:topics], options[:output])
+end
